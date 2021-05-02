fb6a15314026244b080fcc3c0e0c79ce201b11cb
Fred George <fredgeorge@acm.org>
2021-05-02 13:34:23 -0400

    Fred: RectangleTest without Rectangle implementation

--- exercises/src/rectangle/Rectangle.kt
+++ exercises/src/rectangle/Rectangle.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
 package rectangle
 
 class Rectangle {
--- tests/test/unit/RectangleTest.kt
+++ tests/test/unit/RectangleTest.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
 package unit
 
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -5,8 +11,7 @@ import org.junit.jupiter.api.Test
 import rectangle.Rectangle
 
 private class RectangleTest {
-    @Test
-    fun area() {
+    @Test fun area() {
         assertEquals(24.0, Rectangle(4, 6).area);
     }
 }
